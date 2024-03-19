677186a96b1bd6333a70ac92fc03816b1a31209f
liorthr <liortahar116@gmail.com>
2024-03-19 13:13:58 +0200

    DogInfos: <Owner/> component done

--- frontend/src/components/DogInfo/owner/Owner.jsx
+++ frontend/src/components/DogInfo/owner/Owner.jsx
@@ -1,8 +1,52 @@
 import './owner.css'
+import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot} from '@fortawesome/free-solid-svg-icons';
+
+
+
 const Owner = () => {
+
+    const ownerData = [
+        {
+            id_user: 1,
+            id_dogs: 2,
+            name: 'Omer Adam',
+            location: 'Savyon',
+        },
+        {
+            id_user: 2,
+            id_dogs: 2,
+            name: 'Eyal Golan',
+            location: 'Tel Aviv',
+        },
+        {
+            id_user: 3,
+            id_dogs: 2,
+            name: 'Eden Hasson',
+            location: 'Jerusalem',
+        },
+    ]
+    const owner = ownerData.find(owner => owner.id_user === 1);
+
     return(
         <>
-        Owner
+        {owner && ( // Check if the owner is found
+            <div className='owner_box'>
+                <div className="owner_top">
+                     <h3 style={{fontWeight: 'bold'}}>Owner informations</h3>
+                    <Separator />
+                    <h5>{owner.name}</h5>
+                        <div className="location-info">
+                            <FontAwesomeIcon icon={faLocationDot} /><h6>{owner.location}</h6>
+                        </div>
+                    </div>
+                <div className="owner_bottom">
+                <Button variant="outline" style={{backgroundColor: '#fbc500', borderRadius: '16px'}}>Meet the owner</Button>
+                </div>
+            </div>
+        )}
         </>
     )
 }
